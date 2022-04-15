cf4a0df72c1b6c1f30e91fe30ab6c826f65854b3
DmitrySergienko <dmitrisergienko@gmail.com>
2022-04-15 12:26:16 +0300

    Initial commit

---
+++ app/src/main/java/ru/ds/gitapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.ds.gitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.ds.gitapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
